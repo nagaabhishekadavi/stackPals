2665e7561a8125d9edd33607e4147f313d5b49ac
Naga Abhishek Adavi <naga.abhishek.adavi>
2026-01-01 16:47:10 +0530

    First push

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("This is my first node project")
+
+const express=require('express')
+const app=express();
+
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from server")
+})
+app.listen(3000,()=>{
+    console.log("Listening ")
+})
