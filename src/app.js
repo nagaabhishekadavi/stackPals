3b2d889632957c1dd43310da13c6319dd4860ad7
Naga Abhishek Adavi <naga.abhishek.adavi>
2026-01-01 17:15:30 +0530

    Adding basic code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ console.log("This is my first node project")
 const express=require('express')
 const app=express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Abhishek",lastName:"Adavi"})
+})
 
+app.post("/user",(req,res)=>{
+    console.log("");
+    res.send("Data has been succesfully added")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data has been deleted")
+})
 app.use("/test",(req,res)=>{
     res.send("Hello from server")
 })
